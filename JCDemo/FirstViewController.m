e78f16b0a37147dd6c9a8fadd473d8652eadeda8
mac <mac@macdemacbook-pro.local>
2015-11-20 14:24:31 +0800

    Initial Commit

---
+++ JCDemo/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  JCDemo
+//
+//  Created by mac on 15/11/20.
+//  Copyright © 2015年 Jcdroid. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
